c91feca08f8e1a2a1b014392e110336b817cf941
solerji <2474499153@qq.com>
2019-08-27 23:50:01 +0800

    article.js update

--- routes/api/timeline.js
+++ routes/api/timeline.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../db/db.js')
+const connection = require('../db/mysqldb.js')
+// const db = require('../db/db.js')
 
 // 获取时间轴时间和标题
 router.get('/api/timeline', (req, res) => {
-    db.Article.find({isPublish: true}).distinct('updateTime','title', (err, doc) => {
-        if (err) {
-            console.log(err)
-        } else if (doc) {
-            res.send(doc)
-        }
-    })
+  db.Article.find({ isPublish: true }).distinct(
+    'updateTime',
+    'title',
+    (err, doc) => {
+      if (err) {
+        console.log(err)
+      } else if (doc) {
+        res.send(doc)
+      }
+    }
+  )
 })
 
 module.exports = router
