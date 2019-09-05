c7aa67df339d0b427c68ba7286c29f1525bb4410
solerji <2474499153@qq.com>
2019-09-05 23:04:31 +0800

    add delete interface

--- routes/api/article.js
+++ routes/api/article.js
@@ -2,19 +2,55 @@ const express = require('express')
 const router = express.Router()
 const connection = require('../db/mysqldb.js')
 
-// // 发布文章
-// router.post('/api/article', (req, res) => {
-//   const article = {
-//     title: req.body.title,
-//     author: req.body.author,
-//     content: req.body.content,
-//     createTime: Date(),
-//     tags: req.body.tags,
-//     isPublish: true
-//   }
-//   new db.Article(article).save()
-//   res.status(200).send('succeed in saving new passage.')
-// })
+// 发布文章
+let addSql =
+  'INSERT INTO article( title, author, tags, createTime, content) VALUES (?,?,?,?,?,?)'
+router.post('/api/article', (req, res) => {
+  let addSqlParams = [
+    req.query.title,
+    req.query.author,
+    req.query.tags,
+    req.query.createTime,
+    req.query.content
+  ]
+  connection.query(addSql, addSqlParams, function(err, doc) {
+    if (err) {
+      console.log('[发布失败！] - ', err.message)
+      return
+    } else if (doc) {
+      console.log('发布成功 ID：', doc.aid)
+      console.log('发布成功：', doc)
+    } else {
+      res.status(404).send(err.message)
+    }
+  })
+})
+
+// 获取文章列表
+router.get('/api/articles', (req, res) => {
+  connection.query('SELECT aid, title, content FROM article', function(
+    err,
+    article
+  ) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message)
+      return
+    } else if (article) {
+      // if (aid) {
+      // const token = creatToken(doc._id, doc.name)
+      console.log(article)
+      res.status(200).send({
+        list: article
+      })
+      // } else {
+      //   res.send('no data!')
+      // }
+    } else {
+      res.status(404).send('no data!')
+      // res.send(err.message)
+    }
+  })
+})
 
 // 获取某篇文章
 router.get('/api/article', (req, res) => {
@@ -39,62 +75,38 @@ router.get('/api/article', (req, res) => {
 })
 
 // 删除文章
-router.delete('/api/article', (req, res) => {
-  connection.query(
-    "DELETE FROM article where article.aid = '${req.query.aid}'",
-    function(err, doc) {
-      if (err) {
-        console.log('[SELECT ERROR] - ', err.message)
-        return
-      } else if (doc) {
-        console.log(doc)
-        res.status(200).send('删除成功')
-      } else {
-        res.status(404).send(err.message)
-      }
+router.delete('/api/delArticle', (req, res, next) => {
+  console.log(req)
+  var aid = req.body.aid
+  connection.query('DELETE FROM article where aid = ?', aid, function(
+    err,
+    doc
+  ) {
+    if (err) {
+      console.log('[删除失败！] - ', err.message)
+      return
+    } else if (doc) {
+      console.log(doc.affectedRows)
+      res.status(200).send('删除成功')
+    } else {
+      res.status(404).send(err.message)
     }
-  )
+  })
 })
 
-// // 修改文章（或者搞懂patch）
-// router.patch('/api/article', (req, res) => {
-//   const aid = req.params.aid
-//   const article = {
-//     title: req.body.title,
-//     author: req.body.author,
-//     tags: req.body.tags,
-//     updateTime: Date(),
-//     content: req.body.content,
-//     isPublish: true
-//   }
-//   db.Article.update({ aid: aid }, article, (err, data) => {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       res.status(200).send('succeed in updating ---' + data.title)
-//     }
-//   })
-// })
-
-// 获取文章列表
-router.get('/api/articles', (req, res) => {
-  connection.query('SELECT aid, title FROM article', function(err, article) {
+let updateSql =
+  'UPDATE article SET title = ?, author = ?, tags = ?, updateTime = ?, content = ?'
+// let updateSqlParams = [req.query.title,req.query.author,req.query.tags,req.query.updateTime,req.query.content]
+// 更新文章
+router.post('/api/updateArticle', (req, res) => {
+  connection.query(updateSql, updateSqlParams, function(err, doc) {
     if (err) {
-      console.log('[SELECT ERROR] - ', err.message)
+      console.log('[更新失败！] - ', err.message)
       return
-    } else if (article) {
-      // if (aid) {
-      // const token = creatToken(doc._id, doc.name)
-      console.log(article)
-      res.status(200).send({
-        list: article
-      })
-      // } else {
-      //   res.send('no data!')
-      // }
+    } else if (doc) {
+      console.log('更新成功 ID：', doc.aid)
     } else {
-      res.status(404).send('no data!')
-      // res.send(err.message)
+      res.status(404).send(err.message)
     }
   })
 })
